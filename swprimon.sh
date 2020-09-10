5220f652344060f06afb748d934f77c43d50f8a3
Badr Ibrahim <badr.ibrahim@prounix.de>
2020-09-10 15:30:22 +0200

    Create master branch

--- swprimon.sh
+++ swprimon.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
 
 echo "select new primary monitor:"
-cmd=$(pxrandr --listactivemonitors | sed -E "{/Monitors/d;s/^\s[0-9]:\s*\+(.?[A-Z]*.?)\s*(.*)/- \1/g}")
-echo $cmd
-
-count=$(echo $cmd | grep -v '*' | grep -c)
-
-for i in $count
-
-
+xrandr --listactivemonitors | sed -E "{/Monitors/d;s/^\s[0-9]:\s*\+(.?[A-Z]*.?)\s*(.*)/- \1/g}"
 read -p "Enter Monitor name: " NEWPRIMARY
 xrandr --output $NEWPRIMARY --primary
 
